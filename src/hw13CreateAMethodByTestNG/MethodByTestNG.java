17c57c13113579e2e9756e03ebaef7628e1ce026
pinky <pinjoysh@gmail.com>
2022-06-27 04:09:36 -0400

    hw14Selenium_useofclick

--- src/hw13CreateAMethodByTestNG/MethodByTestNG.java
+++ src/hw13CreateAMethodByTestNG/MethodByTestNG.java
@@ -6,28 +6,31 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+
 public class MethodByTestNG {
 	WebDriver driver;
+
 	@BeforeTest
 	public void setUp() {
 		System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
-		driver= new ChromeDriver();
+		driver = new ChromeDriver();
+//		driver.getProperties();
+//		driver.setProperties();
 		driver.manage().window().maximize();
 		driver.manage().deleteAllCookies();
-		
-		
+
 	}
+
 	@Test
 	public void urlTest() throws InterruptedException {
 		driver.get("https://www.kroger.com");
 		Thread.sleep(3000);
-	
+
 	}
+
 	@AfterTest
 	public void tearUp() {
 		driver.quit();
 	}
-	
 
-}  
- 
+}
---
+++ src/hw14Selenium/Use_of_click_methods.java
@@ -0,0 +1,56 @@
+package hw14Selenium;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.By.ByClassName;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class Use_of_click_methods {
+	WebDriver driver;
+	@BeforeTest
+	public void setUp() {
+		System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+        driver.get("https://www.kroger.com/signin?redirectUrl=/");
+		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+	}
+	@Test
+	public void signinButtonTest1() throws InterruptedException {
+		driver.findElement(By.id("SignIn-submitButton")).click();  //.clear();
+		Thread.sleep(5000);
+	}
+	public void signButtonTest2() {
+		driver.findElement(By.className("kds-Button kds-Button--primary w-full my-8")).click();;
+	}
+	public void signInEmailFieldTest3() {
+		driver.findElement(By.name("name")).click();
+	}
+	
+	public void macysJobsTest4() {
+		driver.findElement(By.linkText("MacysJOBS")).click();// From Macys
+	}
+	public void macysJobsTest5() {
+		driver.findElement(By.partialLinkText("MacysJ")).click();// From Macys
+	}
+	public void logotest6() {
+		driver.findElement(By.cssSelector("img.Image")).click();
+	}
+	public void signInButtonTest7() {
+		driver.findElement(By.cssSelector("button#SignIn-submitButton")).click();// css Selrctor By Id
+	}
+	
+	
+	@AfterTest
+	 public void tearUp() {
+		 driver.quit();
+	 }
+
+}
